a401f3857f1a13fce74934ce9dee57269affea37
Shimul Mahmud <shimulmahmud18@gmail.com>
2023-01-15 01:23:04 +0600

    full stack client side

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "../Posts/Post/Post";
+
+const Posts = () => {
+  return (
+    <>
+      <div>Posts</div>
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
