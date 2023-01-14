73095b160a93e25e5874031dc922f37942514ea1
Shimul Mahmud <shimulmahmud18@gmail.com>
2023-01-15 01:21:48 +0600

    full stack server side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("fullstack app");
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  process.env.CONNECTION_URL ||
+  "mongodb+srv://shimul:1804021@cluster0.chmcfal.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(CONNECTION_URL, {})
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+  )
+  .catch((err) => console.log(err));
