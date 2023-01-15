20690128a98147da663c8e14ae220c205e543edf
Shimul Mahmud <shimulmahmud18@gmail.com>
2023-01-15 15:40:05 +0600

    full stack client side

--- server/index.js
+++ server/index.js
@@ -1,18 +1,17 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-const CONNECTION_URL =
-  process.env.CONNECTION_URL ||
-  "mongodb+srv://shimul:1804021@cluster0.chmcfal.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5000;
 
