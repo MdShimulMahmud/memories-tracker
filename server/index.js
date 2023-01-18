d04df9f15344e0594cd1457a045c84fdd2e1810b
Shimul Mahmud <shimulmahmud18@gmail.com>
2023-01-18 23:30:11 +0600

    lesson-01 done

--- server/index.js
+++ server/index.js
@@ -3,13 +3,18 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts.js";
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json());
 
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
